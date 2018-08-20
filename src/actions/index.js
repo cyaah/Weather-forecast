51c270c02f6e3bb038e3eb8b37dfbdc223e54b94
Calvin Yah <calvin.yah@gmail.com>
2018-08-20 15:04:09 -0400

    Created a reducer to handle AJAX request to weather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const AP_KEY = 'd9ee20c81247cbc7079cb9bbd49fcc23';
-const root_url = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-
+const API_KEY = 'd9ee20c81247cbc7079cb9bbd49fcc23';
+const root_url = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -10,8 +9,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather (city) {
   const url = `${root_url}&q=${city},us`;
   const request = axios.get(url);	
+
+
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
 		payload: request
 	};
 }
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,13 @@
+import { Fetch_Weather } from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type){
+	case FETCH_WEATHER: 
+	  return state.concat([action.payload.data]);
+	  //The 3 dots represent that it takes all of the entries outisde of it and adds into the new array. Like flattening it
+	  //The most important thing is to not mutate the existing state over time but return a new state 
+	  return [ action.payload.data, ...state];
+	}
+  
+	return state;
+}
