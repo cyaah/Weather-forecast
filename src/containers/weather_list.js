f726d6f23bbc2cac97bd46c9288f1a2db7ee6ce0
Calvin Yah <calvin.yah@gmail.com>
2018-08-21 12:32:12 -0400

    Rendered temperate line graph using sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,22 @@
 import  React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 class WeatherList extends Component {
+  //cityData is essentially each city in the array 
   renderWeather(cityData){
+    const name = cityData.city.name;
+    //Inside each city we are mapping over an array called list which has the forecast.
+    //Weather is the argument which is essentially forecast for each day
+    const temps = cityData.list.map(weather => weather.main.temp)
+    console.log(temps);
     return (
-      <tr>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data ={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
