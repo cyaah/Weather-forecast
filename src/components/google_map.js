06edf7582f84be7b47051a5b06a1f31948705b11
Calvin Yah <calvin.yah@gmail.com>
2018-08-22 14:23:45 -0400

    Added error handling for input of city

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -3,6 +3,7 @@ import  React, { Component } from 'react';
 
 class GoogleMap extends Component {
   componentDidMount() {
+  	//Used reacts ref system in order to get direct dom reference to the div below
   	new google.maps.Map(this.refs.map, {
   		zoom: 12,
   		center: {
